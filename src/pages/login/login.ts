253c5bbdb9f72396fc8e2f5ad59f60c5d15c579c
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2017-11-15 03:55:17 -0800

    generate login/signup pages

---
+++ src/pages/login/login.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { IonicPage } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+
+  constructor() {
+  }
+}
---
+++ src/pages/signup/signup.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { IonicPage } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-signup',
+  templateUrl: 'signup.html',
+})
+export class SignupPage {
+
+  constructor() {
+  }
+}
